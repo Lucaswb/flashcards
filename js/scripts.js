3cf0b1430b2552e5d78b52ff5de88ab9b0c6d59b
Erik Irgens and Lucas Blasdel <erik@>
2019-06-04 14:51:37 -0700

    add all functionality.

---
+++ js/scripts.js
@@ -0,0 +1,97 @@
+$(document).ready(function() {
+  $("#flashcard1").click(function() {
+    $("#flashcard1hide").toggle();
+    $("#flashcard1show").toggle();
+  });
+
+  $("#flashcard2").click(function() {
+    $("#flashcard2hide").toggle();
+    $("#flashcard2show").toggle();
+  });
+
+  $("#flashcard3").click(function() {
+    $("#flashcard3hide").toggle();
+    $("#flashcard3show").toggle();
+  });
+
+  $("#flashcard4").click(function() {
+    $("#flashcard4hide").toggle();
+    $("#flashcard4show").toggle();
+  });
+
+
+  $("#flashcard5").click(function() {
+    $("#flashcard5hide").toggle();
+    $("#flashcard5show").toggle();
+  });
+
+  $("#flashcard6").click(function() {
+    $("#flashcard6hide").toggle();
+    $("#flashcard6show").toggle();
+  });
+
+  $("#flashcard7").click(function() {
+    $("#flashcard7hide").toggle();
+    $("#flashcard7show").toggle();
+  });
+
+  $("#flashcard8").click(function() {
+    $("#flashcard8hide").toggle();
+    $("#flashcard8show").toggle();
+  });
+
+  $("#flashcard9").click(function() {
+    $("#flashcard9hide").toggle();
+    $("#flashcard9show").toggle();
+  });
+
+  $("#flashcard10").click(function() {
+    $("#flashcard10hide").toggle();
+    $("#flashcard10show").toggle();
+  });
+
+  $("#flashcard11").click(function() {
+    $("#flashcard11hide").toggle();
+    $("#flashcard11show").toggle();
+  });
+
+  $("#flashcard12").click(function() {
+    $("#flashcard12hide").toggle();
+    $("#flashcard12show").toggle();
+  });
+
+  $("#flashcard13").click(function() {
+    $("#flashcard13hide").toggle();
+    $("#flashcard13show").toggle();
+  });
+
+  $("#flashcard14").click(function() {
+    $("#flashcard14hide").toggle();
+    $("#flashcard14show").toggle();
+  });
+
+  $("#flashcard15").click(function() {
+    $("#flashcard15hide").toggle();
+    $("#flashcard15show").toggle();
+  });
+
+  $("#flashcard16").click(function() {
+    $("#flashcard16hide").toggle();
+    $("#flashcard16show").toggle();
+  });
+
+  $("#flashcard17").click(function() {
+    $("#flashcard17hide").toggle();
+    $("#flashcard17show").toggle();
+  });
+
+  $("#flashcard18").click(function() {
+    $("#flashcard18hide").toggle();
+    $("#flashcard18show").toggle();
+  });
+
+  $("#flashcard19").click(function() {
+    $("#flashcard19hide").toggle();
+    $("#flashcard19show").toggle();
+  });
+});
